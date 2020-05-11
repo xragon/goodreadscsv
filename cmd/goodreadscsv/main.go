be3677e6cdf8cb381365301675e13c6e26d279b1
Alex Turner <aturner@gmail.com>
2020-05-11 20:37:22 +1000

    building out goodreads package

--- cmd/goodreadscsv/main.go
+++ cmd/goodreadscsv/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/xragon/goodreadscsv/internal/goodreads"
@@ -56,45 +57,64 @@ func main() {
 		i64, _ := strconv.ParseInt(record[7], 10, 32)
 		rating := int32(i64)
 
-		layoutISO := "2006/01/02" // 2020/01/29
+		// layoutISO := "2006/01/02" // 2020/01/29
 
-		var dateread time.Time
+		// var dateread time.Time
 
-		if len(record[14]) > 0 {
-			dateread, _ = time.Parse(
-				layoutISO,
-				record[14])
-		}
+		// if len(record[14]) > 0 {
+		// 	dateread, _ = time.Parse(
+		// 		layoutISO,
+		// 		record[14])
+		// }
 
-		dateadded, _ := time.Parse(
-			layoutISO,
-			record[15],
-		)
+		// dateadded, _ := time.Parse(
+		// 	layoutISO,
+		// 	record[15],
+		// )
 
-		fmt.Println(len(record[14]))
-		fmt.Println(len(record[15]))
-		fmt.Println(dateread, dateadded)
+		// fmt.Println("" + len(record[14]))
+		fmt.Printf("Read: %s\n", record[14])
+		fmt.Printf("Added: %s\n", record[15])
+		// fmt.Println(dateread, dateadded)
 
 		bookLine := goodreads.Book{
 			Title:  record[1],
 			Author: record[2],
-			ISBN:   record[5],
-			ISBN13: record[6],
+			ISBN:   strings.Trim(record[5], "=\""),
+			ISBN13: strings.Trim(record[6], "=\""),
 			Rating: rating,
 			// DateRead:  dateread,
 			// DateAdded: record[15],
-			Status: record[18],
+			DateRead:  parseDate(record[14]),
+			DateAdded: parseDate(record[15]),
+			Status:    record[18],
 		}
 
-		bookLine.DateAdded.Time = dateadded
-		bookLine.DateAdded.Valid = true
+		// bookLine.DateAdded.Time = dateadded
+		// bookLine.DateAdded.Valid = true
 
-		bookLine.DateRead = sql.NullTime{
-			Time:  dateread,
-			Valid: true,
-		}
+		// bookLine.DateRead = sql.NullTime{
+		// 	Time:  dateread,
+		// 	Valid: true,
+		// }
 
 		fmt.Println(bookLine)
 
+	}
+}
+
+func parseDate(date string) sql.NullTime {
+	layoutISO := "2006/01/02" // 2020/01/29
+	if len(date) > 0 {
+		parsedDate, _ := time.Parse(
+			layoutISO,
+			date)
+		return sql.NullTime{
+			Time:  parsedDate,
+			Valid: true,
+		}
+	}
+	return sql.NullTime{
+		Valid: false,
 	}
 }
--- internal/goodreads/goodreads.go
+++ internal/goodreads/goodreads.go
@@ -2,6 +2,13 @@ package goodreads
 
 import (
 	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofrs/uuid"
 )
@@ -20,6 +27,61 @@ type Book struct {
 }
 
 // Import a segment section of a Goodreads CSV in postgresql
-func Import() {
+func Import() error {
+	csvFile, err := os.Open("goodreads_library_export.csv")
 
+	if err != nil {
+		return err
+	}
+	defer csvFile.Close()
+
+	r := csv.NewReader(csvFile)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		bookid, err := uuid.NewV4()
+		if err != nil {
+			return err
+		}
+
+		i64, _ := strconv.ParseInt(record[7], 10, 32)
+		rating := int32(i64)
+
+		bookLine := Book{
+			ID:        bookid,
+			Title:     record[1],
+			Author:    record[2],
+			ISBN:      strings.Trim(record[5], "=\""),
+			ISBN13:    strings.Trim(record[6], "=\""),
+			Rating:    rating,
+			DateRead:  parseDate(record[14]),
+			DateAdded: parseDate(record[15]),
+			Status:    record[18],
+		}
+
+		fmt.Println(bookLine)
+	}
+	return nil
+}
+
+func parseDate(date string) sql.NullTime {
+	layoutISO := "2006/01/02" // 2020/01/29
+	if len(date) > 0 {
+		parsedDate, _ := time.Parse(
+			layoutISO,
+			date)
+		return sql.NullTime{
+			Time:  parsedDate,
+			Valid: true,
+		}
+	}
+	return sql.NullTime{
+		Valid: false,
+	}
 }
